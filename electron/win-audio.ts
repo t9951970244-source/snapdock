9302422e56cb961aa3b186cb21cc04e2f2f920a4
t9951970244-source <t9951970244@gmail.com>
2026-09-17 11:14:49 +0300

    Add files via upload

--- electron/win-audio.ts
+++ electron/win-audio.ts
@@ -161,7 +161,7 @@ try {
   Add-Type -AssemblyName System.Runtime.WindowsRuntime
   $asTaskGeneric = ([System.WindowsRuntimeSystemExtensions].GetMethods() | Where-Object {
     $_.Name -eq 'AsTask' -and $_.GetParameters().Count -eq 1 -and
-    $_.GetParameters()[0].ParameterType.Name -eq 'IAsyncOperation`1' })[0]
+    $_.GetParameters()[0].ParameterType.Name -eq ('IAsyncOperation' + [char]96 + '1') })[0]
   function Await($task, $type) {
     $m = $asTaskGeneric.MakeGenericMethod($type)
     $t = $m.Invoke($null, @($task)); $t.Wait(-1) | Out-Null; $t.Result
